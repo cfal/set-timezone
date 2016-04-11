224800393f8eed549d0fb084122a22ec33c9382d
Alex Lau <alau@windows95.local>
2016-04-10 22:33:23 -0700

    Add Date.parse and Date.now

--- src/set-timezone.js
+++ src/set-timezone.js
@@ -2,5 +2,8 @@ var time = require('set-timezone-time');
 
 module.exports = function(timezone) {
     time.tzset(timezone);
+    var origDate = Date;
     Date = time.Date;
+    Date.parse = origDate.parse.bind(origDate);
+    Date.now = function() { return new time.Date(); };
 };
