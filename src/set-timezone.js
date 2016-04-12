aae6766b16e853a92bf405427856adf7ccf4cb5d
Alex Lau <alau@windows95.local>
2016-04-11 20:04:11 -0700

    Set timezone for Date.parse

--- src/set-timezone.js
+++ src/set-timezone.js
@@ -1,9 +1,18 @@
 var time = require('set-timezone-time');
 
 module.exports = function(timezone) {
+    var origOffset = new Date().getTimezoneOffset();
     time.tzset(timezone);
+    var newOffset = (new time.Date().getTimezoneOffset() - origOffset) * 60 * 1000;
     var origDate = Date;
     Date = time.Date;
-    Date.parse = origDate.parse.bind(origDate);
+    Date.parse = function(s) {
+        var d = origDate.parse.apply(origDate, arguments);
+        if (!isNaN(d) && !/^\d{4}\-[01]\d\-[0-3]\d/.test(s)) {
+            // Offset Non-ISO dates
+            d += newOffset;
+        }
+        return d;
+    };
     Date.now = function() { return new time.Date(); };
 };
